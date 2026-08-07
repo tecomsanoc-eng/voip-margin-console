03fee2e72100c4e8de23e2ead0e7876b38785ade
Tecomsa VOIP <tecomsanoc@gmail.com>
2026-08-07 10:52:04 +0300

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -12,5 +12,5 @@
  */
 window.SUPABASE_CONFIG = {
   url: 'https://skqawjqabuhrgzpypkrx.supabase.co',
-  anonKey: 'sb_publishable_uWbs8DhlSGsUh-V8LwSRFQ_6j_9N3gD'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrcWF3anFhYnVocmd6cHlwa3J4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwMjk0NTgsImV4cCI6MjEwMTYwNTQ1OH0.VWfOY2tbA8esF-Qqc_gAO2xm1LYjo0Rd2yqX-OaZKZ4'
 };
